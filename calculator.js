c6fd123a7004108772535e7ca8519a7073ffd1ff
runtyalien <omgadkar@gmail.com>
2024-12-17 23:21:42 +0530

    Feat: Handle two numbers

--- calculator.js
+++ calculator.js
@@ -1,8 +1,9 @@
 export function add(numbers){
     if (numbers === "") return 0;
-    return parseInt(numbers);
+    const numArray = numbers.split(",");
+    return numArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
 
-const number = "1";
+const number = "1,2";
 
 console.log(add(number));
